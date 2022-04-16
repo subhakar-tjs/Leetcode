643918ed403a221ec1d31e66a361d492b2f79533
subhakar-tjs <71774625+subhakar-tjs@users.noreply.github.com>
2022-04-16 23:04:06 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (26.66%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-   
-    int helper(int m, int n)
+    int countPaths(int i,int j,int n,int m,vector<vector<int>> &dp)
     {
-        int dp[m][n];
-        memset(dp,0,sizeof(dp));
-            for(int i=0;i<m;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    if(i==0||j==0)
-                    dp[i][j]=1;
-                    else 
-                   dp[i][j]=dp[i][j-1]+dp[i-1][j];
-}
-            }
-        return dp[m-1][n-1];
-           
-        }
-    int uniquePaths(int m, int n) {
+        if(i==(n-1)&&j==(m-1)) return 1;
+        if(i>=n||j>=m) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+     else return dp[i][j]=countPaths(i+1,j,n,m,dp)+countPaths(i,j+1,n,m,dp);
         
-        return helper(m,n);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+       
+        //dp[1][1]=1;
+       int num=countPaths(0,0,m,n,dp);
+        if(m==1&&n==1)
+            return num;
+        return dp[0][0];
     }
 };
